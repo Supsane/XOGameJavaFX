70da6d3e8d3d43ae4e54a41e206076fa101d5106
Supsane <echashurin@gmail.com>
2017-02-28 12:12:49 +0300

    add resoures and MenuBar

--- src/XOJavaFX/ru/geekbrains/Model/MainWindow.java
+++ src/XOJavaFX/ru/geekbrains/Model/MainWindow.java
@@ -2,21 +2,60 @@ package XOJavaFX.ru.geekbrains.Model;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 //http://code.makery.ch/library/javafx-8-tutorial/ru/
 
 public class MainWindow extends Application {
 
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../View/mainWindow.fxml"));
-        primaryStage.setTitle("XOGameJavaFX");
-        primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("XOGameJavaFX");
+
+        initRootLayout();
+
+        showMainWindow();
+
     }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainWindow.class.getResource("view/menuBar.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showMainWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainWindow.class.getResource("view/mainWindow.fxml"));
+            GridPane mainWindow = (GridPane) loader.load();
+            rootLayout.setCenter(mainWindow);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
